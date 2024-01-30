7f1554333ea2610fe147ec8c219e48e05a93e6ed
Michel Pilon <pilon.michel@gmail.com>
2024-01-29 22:56:42 -0500

    SirSqlChauffeur - Version 1.0.0.0 - Fait la livraison de SirSqlValet

---
+++ SirSqlChauffeur/CopyFolder.cs
@@ -0,0 +1,34 @@
+﻿namespace SirSqlChauffeur;
+
+internal static class CopyFolder
+{
+    private const string margin = "    ";
+
+    public static void CopyDirectory(string sourceDir, string destinationDir, bool firstCall = true)
+    {
+        if (firstCall)
+            Console.WriteLine($"\n\rCopie des nouveaux fichiers");
+
+        Console.WriteLine($"{margin}SOURCE {sourceDir.eos(50)}");
+        Console.WriteLine($"{margin}TARGET {destinationDir.eos(50)}");
+
+        // Create the target directory if it does not already exist
+        Directory.CreateDirectory(destinationDir);
+
+        // Copy each file into the new directory
+        foreach (string filePath in Directory.GetFiles(sourceDir))
+            CopyAndLog(filePath, destinationDir);
+
+        // Copy each subdirectory using recursion
+        foreach (string subDirPath in Directory.GetDirectories(sourceDir))
+            CopyDirectory(subDirPath, Path.Combine(destinationDir, Path.GetFileName(subDirPath)), true); // recursive call
+    }
+
+    private static void CopyAndLog (string filePath, string destinationDir)
+    {
+        File.Copy(filePath, Path.Combine(destinationDir, Path.GetFileName(filePath)), true);
+        Console.WriteLine($"{margin}-> {Path.GetFileName(filePath)}");
+    }
+
+    private static string eos (this string s, int l) => s.Length < l ? s : $"...{new string(s.TakeLast(l - 3).ToArray())}";
+}
---
+++ SirSqlChauffeur/Program.cs
@@ -0,0 +1,78 @@
+﻿using   SirSqlChauffeur;
+
+string  processName = "SSMS";
+var     source      = @"\\ccq.org\Partages\SS01\PILON_MICHEL\SirSqlChauffeur\FilesNew";
+var     target      = @"C:\Program Files (x86)\Microsoft SQL Server Management Studio 19\Common7\IDE\Extensions";
+
+Console.Clear();
+Console.WriteLine();
+Console.WriteLine("Sir Sql Chauffeur");
+Console.WriteLine();
+Console.WriteLine();
+
+Action execute = () =>
+{ 
+    if (!new DirectoryInfo(source).Exists)
+    {
+        Console.WriteLine();
+        throw new Exception($"Incapable de repérer le folder suivant :\n\r    {source}");
+    }
+    Console.WriteLine($"Le folder d'update a été repéré");
+
+    if (!Directory.GetFiles(source).Select(_ => new FileInfo(_)).Any())
+    {
+        Console.WriteLine();
+        throw new Exception($"Il n'y a pas de fichiers dans le folder suivant :\n\r    {source}");
+    }
+    Console.WriteLine($"Les fichiers et folders du folder d'update ont été repérés");
+
+    var processes = Process.GetProcessesByName(processName);
+    if (processes.Any())
+    {
+        Console.WriteLine();
+        throw new Exception("SSMS est en cours d'exécution et doit être fermé");
+    }
+
+    if (!Directory.Exists(target))
+    {
+        Console.WriteLine();
+        throw new Exception($"Incapable de repérer le folder suivant :\n\r    {target}");
+    }
+
+    target = Path.Combine(target, "SirSqlValet");
+    if (Directory.Exists(target)) 
+    {
+        try
+        {
+            new DirectoryInfo(target).Delete(recursive: true);
+            Console.WriteLine($"Les fichiers et folders déjà en place ont été détruits.");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine();
+            throw new Exception($"Incapable de repérer le folder suivant :\n\r    {target}");
+        }
+    }
+
+    CopyFolder.CopyDirectory(source, target);
+    Console.WriteLine();
+    Console.WriteLine($"Les nouveaux fichiers et folders ont été copiés.");
+};
+
+try
+{
+    execute();
+    Console.WriteLine("Tout semble s'être déroulé normalement");
+}
+catch (Exception exception)
+{
+    string message = string.Empty;
+    while ((exception = string.IsNullOrWhiteSpace(message) ? exception : exception.InnerException) != null) 
+        message += $"\n\r\n\r - {exception.Message}";
+
+    Console.WriteLine($"\n\r\n\rERREUR(S) NON PRÉVUE(S){message}");
+}
+
+Console.Write("\n\r\n\r...appuyez une touche pour fermer cette fenêtre ... ");
+Console.ReadKey();
+Console.Write("\n\r\n\r");
